a1906f1b229ee0d6ca8674d0a76563ba46be231a
Abdulbasit Adio <94764960+Ayam7273@users.noreply.github.com>
2023-09-09 21:26:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -114,5 +114,21 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+//Testimonies swipe Effect
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
 
 
